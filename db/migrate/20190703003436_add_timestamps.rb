7d5ee7931eb7d137d3b86b1501fe12f55ab7406e
vladi27 <vbulvakhter@gmail.com>
2019-09-03 14:17:57 -0700

    Reset Database

--- db/migrate/20190703003436_add_timestamps.rb
+++ db/migrate/20190703003436_add_timestamps.rb
@@ -1,5 +1,14 @@
 class AddTimestamps < ActiveRecord::Migration[5.2]
   def change
-    add_timestamps(:users)
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :session_token, null: false
+      t.string :email
+      t.string :password_digest, null: false
+
+      t.timestamps
+      
   end
+  add_index :users, :session_token, :username
+end
 end
